cde45b3df94c401fa0cd0073da191e2e4684df96
Santajit Patra <business.santajitpatra@gmail.com>
2023-09-07 03:49:15 +0530

    fix: navbar btn
    navbar btn fixed

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,19 +1,82 @@
+"use client"
+import Link from 'next/link'
 import React from 'react'
+import { FaFacebookF, FaLinkedinIn, FaInstagram, FaTiktok } from "react-icons/fa";
 
+
+const menuItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Services",
+    href: "/services",
+  },
+  {
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+]
 export default function MobileNav() {
-  return (
-    <div>
-          <div className="flex md:hidden">
-              <div className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                  <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
-                  </svg>
 
-                  <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-              </div>
-          </div>
+  return (
+    <div className="absolute inset-x-0 z-20 flex-1 w-full px-6 py-4 mt-4 transition-all duration-300 ease-in-out bg-gray-900">
+      <div className="flex flex-col capitalize text-gray-300">
+
+        {menuItems?.map((item, index) => (
+          <Link
+            className="mt-2 transition-colors duration-300 transform  hover:text-gray-200"
+            key={index}
+            href={item.href}
+          >
+            {item.title}
+          </Link>
+        ))}
+        <div className="flex justify-center mt-6 lg:flex lg:mt-0 lg:mx-10  ">
+
+          <a href="https://www.instagram.com/roysdigitalmedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+            <FaInstagram />
+
+          </a>
+          <a href="https://www.facebook.com/RoysDigitalMedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+            <FaFacebookF />
+
+          </a>
+          <a href="https://www.linkedin.com/company/roys-digital-media/about" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+
+            <FaLinkedinIn />
+          </a>
+          <a href="https://www.tiktok.com/@roysdigitalmedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+
+            <FaTiktok />
+          </a>
+
+        </div>
+
+        <div className="flex justify-center mt-6  ">
+
+          <Link
+            href="/login"
+            className="w-auto px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+            Inquire now!
+          </Link>
+        </div>
+      </div>
+
+
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { FaFacebookF, FaLinkedinIn, FaInstagram, FaTiktok } from "react-icons/fa";
+import { HiOutlineMenuAlt4, HiX } from "react-icons/hi";
+import MobileNav from './MobileNav';
+
 
 const menuItems = [
     {
@@ -38,61 +41,86 @@ interface menuItems {
 }
 
 interface MenuProps {
-    items: menuItems[];    
+    items: menuItems[];
 }
 
 
 const Navbar: React.FC<MenuProps> = ({ items }) => {
+    const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+
     return (
-        <div className=" shadow bg-gray-800">
+        <div className=" shadow bg-gray-900">
             <div className="container px-6 py-4 mx-auto">
                 <div className="lg:flex lg:items-center">
                     <div className="flex items-center justify-between">
                         <a href="/">
-                            <Image width={80}
+                            <Image width={100}
                                 height={50}
                                 alt="Picture of the author"
-                                className='w-auto h-auto -my-2'
+                                className='-my-2 xl:mr-32'
                                 src={"/rdm_logo.png"} />
                         </a>
 
                         {/* <!-- Mobile menu button --> */}
-                        <div className="flex md:hidden">
-                            <div className="text-gray-500 dark:text-gray-200 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="toggle menu">
-                                <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
-                                </svg>
-
-                                <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+                        <div className="flex lg:hidden"
+                            onClick={() => setShowMobileMenu(!showMobileMenu)}
+                        >
+                            <div className="text-gray-200 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="toggle menu">
+                                {showMobileMenu ? <HiX size={30} />
+                                    : <HiOutlineMenuAlt4 size={30} />}
                             </div>
                         </div>
                     </div>
+                    {showMobileMenu && (
+                        <MobileNav />
+                    )}
 
-                    <div className="absolute inset-x-0 z-20 flex-1 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center lg:justify-between">
-                        <div className="flex flex-col capitalize text-gray-300 lg:flex lg:px-16 lg:-mx-4 lg:flex-row lg:items-center">
+                    <div className="relative inset-x-0 z-20 flex-1 mt-0 p-0 top-0  w-auto flex items-center justify-between">
+                       
+                        <div className="hidden capitalize text-gray-300 lg:flex pl-20 xl:pl-72  flex-row lg:items-center">
 
-                            
                             {menuItems?.map((item, index) => (
                                 <Link
-                                    className="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200"
+                                    className="mt-0 lg:mx-4 hover:text-gray-200"
                                     key={index}
                                     href={item.href}
                                 >
                                     {item.title}
                                 </Link>
                             ))}
+                            <div className="flex justify-center lg:mt-0 lg:mx-2  ">
+
+                                <a href="https://www.instagram.com/roysdigitalmedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+                                    <FaInstagram />
+
+                                </a>
+                                <a href="https://www.facebook.com/RoysDigitalMedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+                                    <FaFacebookF />
+
+                                </a>
+                                <a href="https://www.linkedin.com/company/roys-digital-media/about" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+
+                                    <FaLinkedinIn />
+                                </a>
+                                <a href="https://www.tiktok.com/@roysdigitalmedia" target="_blank" rel="noopener noreferrer" className="mx-2 transition-colors duration-300 transform text-gray-300 hover:text-gray-300" >
+
+                                    <FaTiktok />
+                                </a>
+
+                            </div>
+
+                            <div className="flex justify-center mt-0 xl:-mr-10 w-52 ">
+
+                                <Link
+                                    href="/login"
+                                    className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"                                >
+                                    Inquire now!
+                                </Link>
+
+                            </div>
                         </div>
 
-                        <div className="flex justify-center mt-6 lg:flex lg:mt-0 lg:-mx-2">
-                         
-                            {/* <a href="#" className="mx-2 text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Facebook">
-                                <img className='w-5 text-green-500 ' src="/svg/instagram.svg" alt="img" />
-                            </a>   */}
-                            
-                            <FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#3f5a88", }} />
-                        </div>
+
                     </div>
                 </div>
             </div>
