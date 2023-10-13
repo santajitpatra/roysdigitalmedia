9ff5f552cf6c6ecfb2ba99005abb5d7b5400b8b6
Santajit Patra <business.santajitpatra@gmail.com>
2023-10-13 16:28:42 +0530

    feat: careers config
    careers config array added

---
+++ config/careers.js
@@ -0,0 +1,36 @@
+export const careers = [
+  {
+    name: "Qualifications",
+    category: [
+      "Undergraduate degree (preferred but not limited to)",
+      "Ideal majors (preferred but not limited to)",
+      "No prior experience required",
+    ],
+  },
+  {
+    name: "Positions",
+    category: [
+      "Social Media Analyst",
+      "Web Designer",
+      "Sales Consultant",
+      "Content Manager",
+      "Photographer | Videographer",
+      "Personal Assistant",
+      "Intern",
+    ],
+  },
+  {
+    name: "Skill Set",
+    category: [
+      "Creative",
+      "Organized",
+      "Time-management",
+      "Writing",
+      "Communication",
+      "Data Analysis & Analytics",
+      "Problem-Solving",
+      "Interpersonal Skills",
+      "Social Media",
+    ],
+  },
+];
