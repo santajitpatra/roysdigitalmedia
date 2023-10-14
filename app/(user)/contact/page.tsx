3cd13d8d6c90622844b89d97f5eba90aa3ed688e
Santajit Patra <business.santajitpatra@gmail.com>
2023-10-14 23:55:33 +0530

    feat :  Contact Details
    Contact Details Page added

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -1,7 +1,8 @@
+import ContactDetails from '@/components/ContactDetalis'
 import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div><ContactDetails/></div>
   )
 }
